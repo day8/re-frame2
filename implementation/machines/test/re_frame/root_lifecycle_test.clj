38e8b700366bc1e6ca26f5c26b8f2601027a247f
Mike Thompson <mike.thompson@day8.com.au>
2026-09-25 09:58:05 +1000

    test(machines): root lifecycle test loads the machines artefact itself

--- implementation/machines/test/re_frame/root_lifecycle_test.clj
+++ implementation/machines/test/re_frame/root_lifecycle_test.clj
@@ -21,6 +21,7 @@
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [re-frame.core :as rf]
             [re-frame.frame :as rf.frame]
+            [re-frame.machines]
             [re-frame.machines.test-support :as rf.machines.test-support]
             [re-frame.substrate.plain-atom :as rf.substrate.plain-atom]))
 
